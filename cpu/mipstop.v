911ac412b07779784398aab8c5ab94cabe3885c0
mschneider90 <michael.schneider90@gmail.com>
2014-07-23 20:01:00 -0700

    cpu comment removal

--- cpu/mipstop.v
+++ cpu/mipstop.v
@@ -1,16 +1,9 @@
-//------------------------------------------------
-// top.v
-// David_Harris@hmc.edu 9 November 2005
-// Top level system including MIPS and memories
-//------------------------------------------------
-
 module top(input         clk, reset, 
            output [31:0] writedata, dataadr, 
            output        memwrite);
 
   wire [31:0] pc, instr, readdata;
   
-  // instantiate processor and memories
   mips mips(.clk(clk), 
             .reset(reset),
 				.pc(pc),
