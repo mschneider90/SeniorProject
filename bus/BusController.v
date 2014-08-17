3d796c3dc0281ad4eb3f1525a979974e57d59751
mschneider90 <michael.schneider90@gmail.com>
2014-08-17 14:41:08 -0700

    Add bus controller

---
+++ bus/BusController.v
@@ -0,0 +1,11 @@
+`timescale 1ns / 1ps
+
+//Supports up to 8 devices on a bi-directional bus
+
+module BusController #(parameter ADDR_WIDTH = 16,
+                       parameter DATA_WIDTH = 16)
+                       ();
+                       
+
+
+endmodule
