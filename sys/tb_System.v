4ce39f252d8ee99e8793b96cc2ea56a3179afdbf
mschneider90 <michael.schneider90@gmail.com>
2014-09-11 17:31:36 -0700

    Add System testbench

---
+++ sys/tb_System.v
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+
+module tb_System();
+
+reg clk50MHz;
+reg reset;
+
+System sys(.clk50MHz(clk50MHz),
+           .reset(reset));
+
+initial begin
+    reset = 1;
+    #10
+    reset = 0;
+end
+
+always begin
+    #5 clk = ~clk;
+end
+
+
+
+
+endmodule
