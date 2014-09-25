e7f4222e820e8fb2fdde867af188411badd6203d
Michael <michael.schneider90@gmail.com>
2014-09-24 19:37:40 -0700

    System mostly works in simulation & on board

--- sys/System.v
+++ sys/System.v
@@ -1,25 +1,40 @@
 `timescale 1ns / 1ps
 
-module System(input clk50MHz,
-              input reset);
-
-parameter D_WIDTH = 32;
-parameter A_WIDTH = 32;
+module System #(parameter A_WIDTH = 32,
+                parameter I_WIDTH = 32,
+                parameter D_WIDTH = 16)
+             (input clk50MHz,
+              input reset,
+              input [4:0] debug_ra4,
+              output [7:0] debug_rd4,
+              output mclk, //begin SRAM specific signals
+              output [A_WIDTH-1:0] maddr,
+              output madv_L,
+              output mce_L,
+              output moe_L,
+              output mwe_L,
+              input  mwait,
+              output [D_WIDTH-1:0] bus_data,
+              output mcre,
+              output mub_L,
+              output mlb_L //end SRAM specific signals
+              );
+              
 
 parameter NUM_DEVICES = 8;
 
 //each device gets an ID
 //note that these should be synced with the values in BusAddressTranslator.v
 //There might be a better way to do this???
-parameter ROM_BUS_ID = 0;
-parameter RAM_BUS_ID = 1;
+parameter RAM_BUS_ID = 0;
+parameter ROM_BUS_ID = 1;
 parameter VGA_BUS_ID = 2;
 parameter PS2_BUS_ID = 3;
 parameter ACP_BUS_ID = 4;
 parameter CPU_BUS_ID = 7;
 
-wire [D_WIDTH-1:0] pc;
-wire [D_WIDTH-1:0] instr;
+wire [I_WIDTH-1:0] pc; //TODO how wide should this be?
+wire [I_WIDTH-1:0] instr;
 
 wire bus_wait;
 wire [NUM_DEVICES-1:0] bus_ack;
@@ -27,8 +42,8 @@ wire [NUM_DEVICES-1:0] bus_req;
 wire bus_write;
 wire [1:0] bus_burst_length;
 wire [A_WIDTH-1:0] bus_addr;
-wire [D_WIDTH-1:0] bus_data;
 wire [NUM_DEVICES-1:0] device_en;
+wire [A_WIDTH-1:0] bus_phys_addr;
 
 mips cpu(.clk(clk50MHz),
          .reset(reset),
@@ -40,22 +55,12 @@ mips cpu(.clk(clk50MHz),
          .bus_burst_length(bus_burst_length), //2 bits
          .bus_addr(bus_addr), //32 bits
          .bus_req(bus_req[CPU_BUS_ID]),
-         .bus_data(bus_data)); //16 bits
+         .bus_data(bus_data), //16 bits
+         .debug_ra4(debug_ra4),
+         .debug_rd4(debug_rd4));
 
-imem instr_mem(.addr(pc),
+imem instr_mem(.addr(pc[7:2]),
                .data_r(instr));
-
-//TODO signals to memory should go outside of System for implementation
-wire [23:0] maddr;
-wire moe_L;
-wire mwe_L;
-wire madv_L;
-wire mclk;
-wire mub_L;
-wire mlb_L;
-wire mce_L;
-wire mcre;
-wire mwait;
 
 micron_controller mem_ctrl(.clk50MHz(clk50MHz),
                            .baddr(bus_phys_addr), //32 bits
@@ -74,22 +79,11 @@ micron_controller mem_ctrl(.clk50MHz(clk50MHz),
                            .mcre(mcre),
                            .mwait(mwait));
 
-micron_sram ram(.addr(maddr), // 24 bits
-                .adv_L(madv_L),
-                .ce_L(mce_L),
-                .oe_L(moe_L),
-                .we_L(mwe_L),
-                .mem_wait(mwait),
-                .data(bus_data), //16 bits
-                .ub_L(mub_L),
-                .lb_L(mlb_L));
-
-BusAddressTranslator bat(.virtual_addr(bus_addr), //32 bits
-                         .phys_addr(bus_phys_addr),
-                         .device_en(device_en)); //32 bits
-
 BusController bus_ctrl(.req(bus_req),
                        .clk(clk50MHz),
-                       .ack(bus_ack)); 
+                       .ack(bus_ack),
+                       .virtual_addr(bus_addr),
+                       .phys_addr(bus_phys_addr),
+                       .device_en(device_en)); 
 
 endmodule
