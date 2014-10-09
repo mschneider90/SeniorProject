5d32dec6ad49a6300ad9257e8816530cc8a4bc7d
Michael <michael.schneider90@gmail.com>
2014-10-09 15:15:38 -0700

    Fix ctrlOutMuxSel synthesis inferred latch warning. Testbench OK.

--- bus/BusController.v
+++ bus/BusController.v
@@ -282,6 +282,7 @@ always@(*) begin
             addr_mux_sel <= DATA;
             resetSlaveDevice <= 1;
             writeSlaveDevice <= 0;
+            ctrlOutMuxSel <= ORIGINAL_CTRL;
             pri_en <= 1;
             writeInitiatingDevice <= 0;
             writeBurstLength <= 0;
