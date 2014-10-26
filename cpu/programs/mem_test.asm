ae68aeb4d61a184f1005a109fdf722ea021acb23
Michael <michael.schneider90@gmail.com>
2014-10-25 19:15:28 -0700

    Misc System updates

--- cpu/programs/mem_test.asm
+++ cpu/programs/mem_test.asm
@@ -2,7 +2,7 @@
 .globl main 
 .text  
 main:  
-  addi $a0, $zero, 5
-  addi $sp, $zero, 0
+  addi $a0, $zero, 0x0FE9
+  addi $sp, $zero, 30
   sw $a0, 0($sp)   # store $a0 
-  lw $t0, 0($sp)   # restore $a0  into $t0
+  sw $t0, 4($sp)   # store $a0 into mem 4
