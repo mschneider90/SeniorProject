a76f132d63997d67fd29c3b19e2961c863ffdd52
Michael <michael.schneider90@gmail.com>
2014-11-12 21:33:31 -0800

    Revert change to multiplier - back to 64 bit output

--- cpu/multiplier.v
+++ cpu/multiplier.v
@@ -1,13 +1,12 @@
 `timescale 1ns / 1ps
 
-module multiplier #(parameter width = 16,
-                    parameter out_width = 32)
+module multiplier #(parameter width = 32)
                    (input [width-1:0] in_A,
-						  input [width-1:0] in_B,
-						  input write_en, 
-						  input clk,
-						  output [out_width-1:0] out_hi,
-						  output [out_width-1:0] out_lo);
+				    input [width-1:0] in_B,
+				    input write_en, 
+				    input clk,
+				    output [width-1:0] out_hi,
+					output [width-1:0] out_lo);
 						  
 reg [out_width-1:0] hi;
 reg [out_width-1:0] lo;
@@ -17,8 +16,7 @@ assign out_lo = lo;
 						  
 always@(posedge clk) begin
 	if (write_en) begin
-        hi <= in_A * in_B;
-        lo <= in_A * in_B;
+        {hi, lo} = in_A * in_B;
     end
 end
 
