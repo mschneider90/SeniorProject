166596d6961c36518f8fcfbc560704fd14c9a8e1
Michael Schneider <michael.schneider90@gmail.com>
2014-12-01 22:37:58 -0800

    Minor changes

--- game/uart/mooseboxgame/MooseBoxGame.cs
+++ game/uart/mooseboxgame/MooseBoxGame.cs
@@ -354,7 +354,7 @@ namespace MooseBoxGame
             {
                 try
                 {
-                    uart.write(1050, 0, true);
+                    uart.write(1050, 0);
                     initialized = true;
                 }
                 catch (TimeoutException e)
